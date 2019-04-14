d499ced291184652f95297a57da64db796256434
vidigas <vidigas@bitbucket.org>
2019-04-14 14:56:55 -0300

    first commit

--- app/App.js
+++ app/App.js
@@ -10,30 +10,47 @@ export default class App extends Component {
 		this.state = {
 			show: false,
 			loggedIn: false,
-			selecteds: [],
-			contacts: [],
+			allChats:[],
+			newMessages: [],
 			msg: ''
 		};
 	}
 
 	componentWillMount() {
 		this.icons = JSON.parse(window.sessionStorage.getItem('tv-icons'));
+
+		//check login
 		this.authVerify = setInterval(() => {
 			this.setState({ loggedIn: window.WAPI.isLoggedIn() });
 		}, 1000);
+
+		//getAllchats
+
+		this.allChats = setInterval(() => {
+			this.setState({ allChats: 	window.WAPI.getAllChatsWithNewMsg() })
+		}, 1000)
+
 	}
 
-	componentDidMount() {
+	componentDidMount() {		
+
 		document.addEventListener("keyup", (e) => {
-			console.log('dispatch event');
 			if(e.keyCode === 27) this.setState({ show: false });
 		}, false);
+
+
 	}
 
 	componentWillUpdate(nextProps, nextState) {
 		if(!this.state.loggedIn && nextState.loggedIn) {
-			this.setState({ contacts: window.WAPI.getMyContacts() })
 			clearInterval(this.authVerify) 
+
+		}
+
+		if(nextState.allChats.length){
+			console.log('allChats with new Messages', nextState.allChats)
+			clearInterval(this.allChats)
+		
 		}
 	}
 
@@ -41,80 +58,16 @@ export default class App extends Component {
 		return <button className='tv-open-button' onClick={() => this.setState({ show: true })}><img src={this.icons.chat} /></button>
 	}
 
-	handleContactClick(id) {
-		let selecteds = [...this.state.selecteds];
-		const index = this.state.selecteds.findIndex(({ user }) => id.user === user);
-
-		if(index === -1) {
-			selecteds.push(id);
-		} else {
-			selecteds.splice(index, 1)
-		}
-
-		this.setState({ selecteds });
-	}
-
-	renderContacts() {
-		return this.state.contacts.map((contact) => {
-			const { name, id } = contact;
-			const active = this.state.selecteds.findIndex(({ user }) => id.user === user) != -1;
-			return (
-				<div className={`card ${active ? '-active' : ''}`} onClick={() => this.handleContactClick(id)}>
-					{ name }
-				</div>
-			)
-		});
-	}
-
-	handleSend() {
-		const { selecteds, msg } = this.state;
-		if(!selecteds.length, !msg) return;
-
-		const promises = [];
-		this.state.selecteds.forEach(id => {
-			window.WAPI.sendMessage2(id, this.state.msg, () => console.log);
-		});
-
-		this.setState({ show: false });
-	}
-
-	handleSearch(e) {
-		const regexp = new RegExp(e.target.value, 'i');
-		const contacts = window.WAPI.getMyContacts().filter(contact => {
-			return regexp.test(contact.name) || regexp.test(contact.id.user);
-		});
-
-		this.setState({ contacts });
-	}
-
 	render() {
-		const { selecteds, show, loggedIn } = this.state;
+
+		const { show, loggedIn } = this.state;
 		if(!show || !loggedIn) 
 			return this.renderOpenButton();
 
 		return (
 			<div className='tv-app'>
 				<div className='modal'>
-					<div className='search-wrapper'>
-						<div className='input-group'>
-							<input placeholder='pequise por nome ou telefone' onChange={this.handleSearch.bind(this)} />
-							<img src={this.icons.searchDark} />
-						</div>
-						<div className='contacts-selected-amount'><span>{selecteds.length}</span> contato{selecteds.length === 1 ? '' : 's'} selecionado</div>
-					</div>	
-					<section className='contact-list'>
-						{ this.renderContacts() }
-					</section>
-					<section id='message-wrapper' className='message-wrapper'>
-						<textarea placeholder='write your message' onChange={(e) => this.setState({ msg: e.target.value })} />
-						<div className='actions'>
-							<div>
-								<img className='icon' src={this.icons.clip} />
-								<img className='icon' src={this.icons.image} />
-							</div>
-							<img className='icon' src={this.icons.send} onClick={() => this.handleSend()} />
-						</div>
-					</section>
+					Hello world 
 				</div>
 			</div>
 		);
